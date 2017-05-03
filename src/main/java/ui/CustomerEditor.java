9447a0787c0d1ee79cd2262a5dd5bafe5c1c415b
ppohlitz <p.pohlitz@searchmetrics.com>
2017-05-03 11:26:48 +0200

    adding ui support

---
+++ src/main/java/ui/CustomerEditor.java
@@ -0,0 +1,7 @@
+package ui;
+
+/**
+ * Created by ppohlitz on 02.05.17.
+ */
+public class CustomerEditor {
+}
---
+++ src/main/java/ui/VaadinUI.java
@@ -0,0 +1,18 @@
+package ui;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+public class DomainConfigUI extends UI{
+
+}
